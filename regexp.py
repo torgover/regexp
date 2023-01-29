0113f454fbd0ea217b33042864e4af4faa6e2faf
Sasha Zhigulin <zhigulsasha@gmail.com>
2023-01-29 17:49:37 +0300

    revise homework

--- regexp.py
+++ regexp.py
@@ -7,14 +7,13 @@ with open ('phonebook.csv', encoding='utf8') as f:
     
 
 def correct_name(rows):
-    for employee in rows:
-        result = ' '.join(employee[0:3]).split(' ')[0:3] + employee[3:7]
-        return result
+    result = [' '.join(employee[0:3]).split(' ')[0:3] + employee[3:7] for employee in rows]
+    return result
         
 
 def delete_duplicates(correct_name_list):
     without_dupli = []
-    for comp in contacts_list:
+    for comp in correct_name_list:
         for employee in correct_name_list:
             if comp[0:2] == employee[0:2]:
                 list_employee = comp
@@ -37,9 +36,9 @@ def new_phone(rows, regular, new):
 
 
 correct_name_list = correct_name(contacts_list)
-without_dupli = delete_duplicates(correct_name_list)
+without_dupli_list = delete_duplicates(correct_name_list)
 regular = r'(\+7|8)+[\s(]*(\d{3,3})[\s)-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})'
-correct_list = new_phone(without_dupli, regular, r'+7(\2)\3-\4-\5')
+correct_list = new_phone(without_dupli_list, regular, r'+7(\2)\3-\4-\5')
 regular_2 = r'(\+7|8)+[\s(]*(\d{3,3})[\s)-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})[\s]*[(доб.\s]*(\d+)[)]*'
 correct_phonebook = new_phone(correct_list, regular_2, r'+7(\2)\3-\4-\5 доб.\6')
 
